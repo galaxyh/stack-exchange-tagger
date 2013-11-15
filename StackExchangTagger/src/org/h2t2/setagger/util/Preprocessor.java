9741548dc742043e6c4a639d86d843b53251e0ae
Isaac <b00902067@ntu.edu.tw>
2013-11-16 01:02:55 +0800

    add remove stop words and stemming utilities

--- StackExchangTagger/src/org/h2t2/setagger/util/Preprocessor.java
+++ StackExchangTagger/src/org/h2t2/setagger/util/Preprocessor.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 
+import com.aliasi.tokenizer.*;
+
 /**
  * StackExchange dataset preprocessor
  * 
@@ -24,8 +26,7 @@ public class Preprocessor {
 		while ((record = reader.readNext()) != null) {
 			record = extractCode(record);
 			record = removeHtmlTag(record);
-			record = removeStopWords(record);
-			record = lemmatize(record);
+			record = getUsefulToken(record);
 			writer.writeNext(record);
 		}
 
@@ -41,11 +42,33 @@ public class Preprocessor {
 		return record;
 	}
 
-	private static String[] removeStopWords(String[] record) {
-		return record;
-	}
+    /**
+      This function removes stop words and applys stemming to the title and body fields.
+      @param record Contain 5 fields: ID, title, body, code, tags.
+      @return 5 fields same as parameter but remove stop words in title and body and also do the stemming.
+      @author Isaac
+    */
+	private static String[] getUsefulToken(String[] record) {
+        RegExTokenizerFactory RTF = new RegExTokenizerFactory("(\\w\\S*\\w)|([a-zA-Z])");
+        LowerCaseTokenizerFactory LTF = new LowerCaseTokenizerFactory(RTF);
+        EnglishStopTokenizerFactory ETF = new EnglishStopTokenizerFactory(LTF);
+        PorterStemmerTokenizerFactory PTF = new PorterStemmerTokenizerFactory(ETF);
+        String token;
 
-	private static String[] lemmatize(String[] record) {
+        char [] chars = record[1].toCharArray();
+        Tokenizer tokenizer = PTF.tokenizer(chars, 0, chars.length);
+        record[1] = "";
+        while((token = tokenizer.nextToken()) != null){
+            record[1] += token + " ";
+        }
+
+        chars = record[2].toCharArray();
+        tokenizer = PTF.tokenizer(chars, 0, chars.length);
+        record[2] = "";
+        while((token = tokenizer.nextToken()) != null){
+            record[2] += token + " ";
+        }
+
 		return record;
 	}
 }
