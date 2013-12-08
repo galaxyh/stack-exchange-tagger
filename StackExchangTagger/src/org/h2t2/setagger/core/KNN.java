1cfb5c8700d595ec808f9f61c38d7a03f81d6d1e
Isaac <b00902067@ntu.edu.tw>
2013-12-08 12:34:43 +0800

    add KNN.java

---
+++ StackExchangTagger/src/org/h2t2/setagger/core/KNN.java
@@ -0,0 +1,120 @@
+package org.h2t2.setagger.core;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+import java.util.TreeMap;
+import java.util.Map;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class KNN implements Model {
+
+    private KnnClassifier knn;
+
+    @Override
+    public void train(String trainFileName, String[] args) {
+        CSVReader reader = new CSVReader(new FileReader(trainFileName), ',', '"', '\0', 0);
+        knn = new KnnClassifier();
+
+        String[] record;
+        while ((record = reader.readNext()) != null) {
+            if(record.length != 5){
+                System.err.println("csv read error");
+                System.exit(1);
+            }
+
+            knn.train(record);
+        }
+
+        reader.close();
+    }
+
+    @Override
+    public void predict(String predictFileName, String outputFileName, String[] args) {
+        CSVWriter writer = new CSVWriter(new FileWriter(output), ',');
+        CSVReader reader = new CSVReader(new FileReader(trainFileName), ',', '"', '\0', 0);
+
+        String[] idTags = {"ID", "Tags"};
+        writer.writeNext(idTags);
+
+        String[] record;
+        while ((record = reader.readNext()) != null) {
+            if(record.length != 4){
+                System.err.println("csv read error");
+                System.exit(1);
+            }
+
+            TreeMap<Double, String[]> nearestNeighbor = knn.classify(record);
+            TreeMap<String, Double> tagRank = new TreeMap<String, Double>();
+            TreeMap<Double, String> maxTags = new TreeMap<Double, String>();
+            for(Map.Entry<Double, String[]> entry : nearestNeighbor) {
+                Double proximity = entry.getKey();
+                Double value;
+                for(String str : entry.getValue()) {
+                    if((value = tagRank.get(str)) != null)
+                        tagRank.put(str, value + proximity);
+                    else
+                        tagRank.put(str, proximity);
+                }
+            }
+            for(Map.Entry<String, Double> entry : tagRank) {
+                maxTags.put(entry.getValue(), entry.getKey());
+            }
+
+            idTags = new String[2];
+            idTags[0] = record[0];
+            idTags[1] = maxTags.pollLastEntry().getValue();
+            idTags[1] += " " + maxTags.pollLastEntry().getValue();
+            idTags[1] += " " + maxTags.pollLastEntry().getValue();
+
+            writer.writeNext(idTags);
+        }
+
+        writer.close();
+        reader.close();
+    }
+
+    @Override
+    public void saveModel(String modelFile) {
+        if (knn == null) {
+            System.out.println("Warning! Model has not been trained yet. Model not Saved!");
+            return;
+        }
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream(modelFile);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(knn);
+            out.close();
+            fileOut.close();
+        }
+        catch(IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    @Override
+    public void loadModel(String modelFile) {
+        try {
+            FileInputStream fileIn = new FileInputStream(modelFile);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            knn = (KnnClassifier)in.readObject();
+            in.close();
+            fileIn.close();
+        }
+        catch(IOException i) {
+            i.printStackTrace();
+        }
+        catch(ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+        }
+    }
+}
