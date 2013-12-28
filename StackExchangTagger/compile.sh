b0bb2f1dd38e2929185c876716a108b3b4516737
galaxyh <code@yullage.com>
2013-12-28 13:00:26 +0000

    Modify compilation script.

--- StackExchangTagger/compile.sh
+++ StackExchangTagger/compile.sh
@@ -1,2 +1,2 @@
-javac -classpath ./lib/commons-lang3-3.1.jar:./lib/opencsv-2.3.jar:./lib/lingpipe-4.1.0.jar:./lib/javacsv-2.1.jar -sourcepath src/org/h2t2/setagger/core/Cooccurrence.java src/org/h2t2/setagger/core/*.java src/org/h2t2/setagger/util/*.java -d ./
+javac -classpath ./lib/hadoop-client-1.2.1.jar:./lib/hadoop-core-1.2.1.jar:./lib/hadoop-tools-1.2.1.jar:./lib/commons-lang3-3.1.jar:./lib/opencsv-2.3.jar:./lib/lingpipe-4.1.0.jar:./lib/javacsv-2.1.jar -sourcepath src/org/h2t2/setagger/core/Cooccurrence.java src/org/h2t2/setagger/core/*.java src/org/h2t2/setagger/util/*.java -d ./
 
---
+++ StackExchangTagger/compile_comr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Coocurrence Map-reduce version compilation script.
+
+if [ ! -d "classes" ]; then
+    echo "Create classes folder..."
+    mkdir classes
+fi
+
+echo "Compiling..."
+javac -classpath ./lib/hadoop-client-1.2.1.jar:./lib/hadoop-core-1.2.1.jar:./lib/hadoop-tools-1.2.1.jar:./lib/commons-lang3-3.1.jar:./lib/opencsv-2.3.jar:./lib/lingpipe-4.1.0.jar:./lib/javacsv-2.1.jar -sourcepath src/org/h2t2/setagger/core/Cooccurrence.java src/org/h2t2/setagger/core/*.java src/org/h2t2/setagger/util/*.java -d classes
+
+echo "Creating JAR file..."
+jar -cvf CoocurrenceMR.jar -C classes/ .
+
+echo "Done."
