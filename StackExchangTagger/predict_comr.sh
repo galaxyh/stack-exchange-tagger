f167f8e865c10895d8e69b7ac6339fdac470584e
steven <code@yullage.com>
2013-12-29 13:17:18 +0800

    Modify predict shell script.

--- StackExchangTagger/predict_comr.sh
+++ StackExchangTagger/predict_comr.sh
@@ -1,1 +1,2 @@
-hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.Tagger -comr /input /output /model/cooccurrence_head_2000.model#cooccurrence_head_2000.model
+hadoop fs -rmr /output
+hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.CooccurrenceMapReduce /input /output /model/cooccurrence_head_2000.model
