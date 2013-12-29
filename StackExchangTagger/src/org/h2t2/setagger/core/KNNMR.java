798b75e07930e9d6b44f98c159dd9d978f8fb8fa
Isaac <b00902067@ntu.edu.tw>
2013-12-29 19:39:13 +0800

    KNNMR.java done

--- StackExchangTagger/src/org/h2t2/setagger/core/KNNMR.java
+++ StackExchangTagger/src/org/h2t2/setagger/core/KNNMR.java
@@ -16,11 +16,12 @@ import java.net.URI;
 
 public class KNNMR {
 
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text> {
+    public static class MyMap extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text> {
         private Path[] localFiles;
         private KnnClassifier knn;
 
         public void configure(JobConf job) {
+            long startTime = System.currentTimeMillis();
             try {
                 localFiles = DistributedCache.getLocalCacheFiles(job);
                 BufferedReader reader = new BufferedReader(new FileReader(localFiles[0].toString()));
@@ -56,13 +57,64 @@ public class KNNMR {
                 System.out.println("IOException");
                 e.printStackTrace();
             }
+
+            System.out.println("configure time: " + (System.currentTimeMillis() - startTime));
         }
 
-        public void map(LongWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
-            String line = value.toString();
+        public void map(LongWritable key, Text text, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+            long startTime = System.currentTimeMillis();
+            String line = text.toString();
             String[] record;
             record = line.split(",");
-            output.collect(new IntWritable(Integer.parseInt(record[0])), new Text(record[1]));
+            if(record.length != 5){ // 5 for test, 4 for real
+                System.err.println("wrong record length");
+                System.exit(1);
+            }
+
+            TreeMap<Double, ArrayList<String>> nearestNeighbor = knn.classify(record);
+
+            TreeMap<String, Double> tagRank = new TreeMap<String, Double>();
+            TreeMap<Double, ArrayList<String>> maxTags = new TreeMap<Double, ArrayList<String>>();
+            for(Map.Entry<Double, ArrayList<String>> entry : nearestNeighbor.entrySet()) {
+                Double proximity = entry.getKey();
+                Double value;
+                for(String str : entry.getValue()) {
+                    if((value = tagRank.get(str)) != null)
+                        tagRank.put(str, value + proximity);
+                    else
+                        tagRank.put(str, proximity);
+                }
+            }
+            ArrayList<String> tmpArrayList;
+            for(Map.Entry<String, Double> entry : tagRank.entrySet()) {
+                if((tmpArrayList = maxTags.get(entry.getValue())) != null) {
+                    tmpArrayList.add(entry.getKey());
+                }
+                else {
+                    tmpArrayList = new ArrayList<String>();
+                    tmpArrayList.add(entry.getKey());
+                    maxTags.put(entry.getValue(), tmpArrayList);
+                }
+            }
+
+            Map.Entry<Double, ArrayList<String>> tmpEntry;
+            StringBuilder sb = new StringBuilder();
+            int cntTags = 0;
+            while((tmpEntry = maxTags.pollLastEntry()) != null) {
+                for(String str : tmpEntry.getValue()) {
+                    sb.append(str).append(" ");
+                    cntTags++;
+                    if(cntTags >= 3) {
+                        break;
+                    }
+                }
+                if(cntTags >= 3) {
+                    break;
+                }
+            }
+
+            output.collect(new IntWritable(Integer.parseInt(record[0])), new Text(sb.toString()));
+            System.out.println("map " + record[0] + " time: " + (System.currentTimeMillis() - startTime));
         }
     }
 
@@ -75,20 +127,22 @@ public class KNNMR {
         conf.setOutputKeyClass(IntWritable.class);
         conf.setOutputValueClass(Text.class);
 
-        conf.setMapperClass(Map.class);
+        conf.setMapperClass(MyMap.class);
         //conf.setCombinerClass(Reduce.class);
         //conf.setReducerClass(Reduce.class);
 
         conf.setInputFormat(TextInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
 
+        conf.setNumReduceTasks(0);
+
         //conf.setKeyFieldComparatorOptions("-n");
         conf.set("mapred.textoutputformat.separator", ",");
 
         FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
 
-        DistributedCache.addCacheFile(new URI("./Train.10w.pre"), conf);
+        DistributedCache.addCacheFile(new URI(args[1]), conf);
 
         JobClient.runJob(conf);
     }
