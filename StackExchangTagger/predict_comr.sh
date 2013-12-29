d1a244a457669a58892cf3c5c0162c73a583e82b
steven <code@yullage.com>
2013-12-29 13:35:59 +0800

    Use relative path

--- StackExchangTagger/predict_comr.sh
+++ StackExchangTagger/predict_comr.sh
@@ -1,2 +1,2 @@
-hadoop fs -rmr /output
-hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.CooccurrenceMapReduce /input /output /model/cooccurrence_head_2000.model
+hadoop fs -rmr output
+hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.CooccurrenceMapReduce input output model/cooccurrence_head_2000.model
