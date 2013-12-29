3ab52a74389e0c9a171d5c8017f17e2ea6af011d
steven <code@yullage.com>
2013-12-29 14:47:29 +0800

    modify model name

--- StackExchangTagger/predict_comr.sh
+++ StackExchangTagger/predict_comr.sh
@@ -1,2 +1,2 @@
 hadoop fs -rmr output
-nohup hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.CooccurrenceMapReduce input output model/cooccurrence_head_2000.model > predict_comr.out 2>&1 &
+nohup hadoop jar CoocurrenceMR.jar org.h2t2.setagger.core.CooccurrenceMapReduce input output model/cooccurrence.model > predict_comr.out 2>&1 &
